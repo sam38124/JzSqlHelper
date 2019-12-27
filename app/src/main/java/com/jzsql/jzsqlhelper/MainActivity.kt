3d671d3d53717fb08ab3265b8bddfb0a89b2092c
sam38124 <sam38124@gmail.com>
2019-12-27 09:14:12 +0800

    v5.1

--- app/src/main/java/com/jzsql/jzsqlhelper/MainActivity.kt
+++ app/src/main/java/com/jzsql/jzsqlhelper/MainActivity.kt
@@ -13,13 +13,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //第一步創建資料庫
         val item= ItemDAO(this,"test.db")
-        //第二步使用ExSql執行資料庫語法
+        //第二步創建資料表
         item.ExSql("CREATE TABLE   IF NOT EXISTS logtable (\n" +
                 "    id   INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
                 "    data VARCHAR NOT NULL,\n" +
                 "    type VARCHAR NOT NULL\n" +
                 ");\n")
-        //第三部使用Query進行資料庫查詢
+        //第三部插入資料
+        item.ExSql("insert into logtable(data,type) values ('hello sql','sql')")
+        //第四部資料查詢
         item.Query("select * from logtable", Sql_Result {
             val result1=it.getString(0)
             val result2=it.getString(1)
